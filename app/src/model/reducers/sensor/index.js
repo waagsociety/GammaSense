957037f233c66dd8c001d0acfc798ff5f6f07b6e
Nathan Uphoff <nathan@Administrators-MacBook-Pro.local>
2017-03-01 14:47:30 +0100

    Removed legacy model data

--- app/src/model/reducers/sensor/index.js
+++ app/src/model/reducers/sensor/index.js
@@ -1,18 +1,3 @@
-const initial = {
-  active: false,
-  history: [],
-}
-
-export function sensor(state = initial, { type, data }) {
-  
-  const update = type === 'sensor'
-    ? Object.assign({}, state, data)
-    : state
-
-  return update
-
-}
-
 export default {
   active: false,
   history: [],
