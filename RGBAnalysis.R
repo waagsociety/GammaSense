05c1ccb9f2ce41e0eb6f6300db19562afed9aa2d
Stefano Bocconi <Stefano.Bocconi@gmail.com>
2017-05-23 10:30:43 +0200

    mean and sd of signal

--- RGBAnalysis.R
+++ RGBAnalysis.R
@@ -22,14 +22,9 @@ func <- function(arg){
 # for i in frameData*.csv; do cat $i | sed 's/\(.*\),$/\1/g' > filt${i};done
 
 
-pixels <- read.csv(fileName)
+pixels <- data.table(read.csv(fileName))
 
-coordX <- ((pixels[,"index"]-1)%/%4)%%dimX + 1
-coordY <- ((pixels[,"index"]-1)%/%4)%/%dimX + 1
-
-pixels <- data.table(frameNR=pixels[,1],index=pixels[,2],coordX,coordY,pixels[,3:5])
-
-#colnames(pixels) <- c("frameNR",colnames(pixels)[2],"X","Y",colnames(pixels)[3:5])
+colnames(pixels) <- c("frameNR",colnames(pixels)[-1])
 
 redPixels <- data.table(pixels[,.N,by=R], key="R")
 
@@ -55,12 +50,20 @@ plot <- ggplot(data = RGBpixels, aes(x=value)) + geom_line(aes(y=func(nr.R)), co
 plot <- plot + ylab(ylabel)
 
 
-setkeyv(pixels,cols=c("coordX","coordY"))
+coordX <- ((pixels[,"index"]-1)%/%4)%%dimX + 1
+coordY <- ((pixels[,"index"]-1)%/%4)%/%dimX + 1
+
+pixelsXY <- data.table(pixels[,1:2],coordX=coordX[,index],coordY=coordY[,index],pixels[,3:5])
+
+#colnames(pixels) <- c("frameNR",colnames(pixels)[2],"X","Y",colnames(pixels)[3:5])
+
+setkeyv(pixelsXY,cols=c("coordX","coordY"))
 
 heatmapProp <- data.table(coordX=rep(1:dimX,dimY), coordY=rep(1:dimY,each=dimX), R=0,G=0,B=0, key=c("coordX,coordY"))
 heatmapBin <- data.table(coordX=rep(1:dimX,dimY), coordY=rep(1:dimY,each=dimX), RGB=0, key=c("coordX,coordY"))
+heatmapBinAll <- data.table(coordX=rep(1:dimX,dimY), coordY=rep(1:dimY,each=dimX), RGB=0, key=c("coordX,coordY"))
 
-indexes <- unique(pixels[,c("coordX","coordY")])
+indexes <- unique(pixelsXY[,c("coordX","coordY")])
 total <- nrow(indexes)
 
 for (i in 1:total){
@@ -68,15 +71,21 @@ for (i in 1:total){
   indexX <- as.integer(indexes[i,1])
   indexY <- as.integer(indexes[i,2])
   
+  subpixels <- pixelsXY[coordX==indexX & coordY==indexY,]
+  
   heatmapProp[coordX==indexX & coordY==indexY,c("R","G","B") := 
-                as.list(pixels[coordX==indexX & coordY==indexY,c(sum(R),sum(G),sum(B))])]
+                as.list(subpixels[,c(sum(R),sum(G),sum(B))])]
   
   heatmapBin[coordX==indexX & coordY==indexY,RGB := 
-                pixels[coordX==indexX & coordY==indexY,.N,] ]
+               subpixels[,.N,] ]
   
-  if (  i%%1000 == 0 ){
-    print(paste("count",i/total*100))
-  }
+  heatmapBinAll[coordX==indexX & coordY==indexY,RGB := 
+                  subpixels[R>0 & G>0 & B>0,.N,] ]
+  
+  
+#  if (  i%%1000 == 0 ){
+#    print(paste("count",i/total*100))
+#  }
   
 }
 
@@ -92,3 +101,39 @@ heatPropRGB <- heatProp + geom_raster(aes(fill = R+G+B)) + scale_fill_gradientn(
 
 heatBinRGB <- ggplot(data = heatmapBin, aes(x = coordX, y = coordY)) +
               geom_raster(aes(fill = RGB)) + scale_fill_gradientn(colours=c("white","black"))
+
+heatBinAllRGB <- ggplot(data = heatmapBinAll, aes(x = coordX, y = coordY)) +
+  geom_raster(aes(fill = RGB)) + scale_fill_gradientn(colours=c("white","black"))
+
+print(heatBinAllRGB)
+
+for (threshold in 1:10*10){
+
+  for (y in 1:(dimY/2)){
+    x <- (y/dimY*dimX)%/%1
+    if (heatmapBinAll[coordX>=x & coordX<=dimX-x & coordY>=y & coordY<=dimY-y,sum(RGB)] < threshold){
+      break;
+    }
+  }
+  print(paste("Threshold:",threshold,"indexes x:",x,"y:",y))
+}
+
+ggplot(data=pixels[,sum(R)+sum(G)+sum(B),by=frameNR], aes(x=frameNR,y=V1)) + geom_line()
+
+tmp <-pixels[,sum(R)+sum(G)+sum(B),by=frameNR][,V1]
+print(paste("Mean:",mean(tmp),"SD:",sd(tmp),"quantile:"))
+quantile(tmp)
+
+ggplot(data=pixels[,.N,by=frameNR], aes(x=frameNR,y=N)) + geom_line()
+
+tmp <-pixels[,.N,by=frameNR][,N]
+print(paste("Mean:",mean(tmp),"SD:",sd(tmp),"quantile:"))
+quantile(tmp)
+
+ggplot(data=pixels[R>0 & G>0 & B>0,.N,by=frameNR], aes(x=frameNR,y=N)) + geom_line()
+
+tmp <-pixels[R>0 & G>0 & B>0,.N,by=frameNR][,N]
+print(paste("Mean:",mean(tmp),"SD:",sd(tmp),"quantile:"))
+quantile(tmp)
+
+ggplot(data=pixels[R>0 & G>0 & B>0 & (R+B+G)>50,.N,by=frameNR], aes(x=frameNR,y=N)) + geom_point()
